deb77cec7b1829bf696f06d6d60702eadc8a010d
LmOpe <lawalmuhammedope@gmail.com>
2025-02-18 03:01:27 +0100

    feat: add sign up endpoint

--- auths/views.py
+++ auths/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"user":serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
